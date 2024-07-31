6736ac10edb2f3163835c590139fdeede941fc7b
Matheus Farias <matheusfariasdasneves@gmail.com>
2024-07-31 18:11:05 -0300

    chore: add shadcn/ui with initial config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{ts,tsx}"],
-  theme: {
-    extend: {},
+export const darkMode = ["class"]
+export const content = [
+  "./pages/**/*.{ts,tsx}",
+  "./components/**/*.{ts,tsx}",
+  "./app/**/*.{ts,tsx}",
+  "./src/**/*.{ts,tsx}",
+]
+export const prefix = ""
+export const theme = {
+  container: {
+    center: true,
+    padding: "2rem",
+    screens: {
+      "2xl": "1400px",
+    },
   },
-  plugins: [],
+  extend: {
+    colors: {
+      background: "#141414",
+    },
+  },
 }
+import tailwindcssAnimate from "tailwindcss-animate"
+
+export const plugins = [tailwindcssAnimate]
