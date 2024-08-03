7b36508f7d08c69e296ab05a71825ef3af03067a
Matheus Farias <matheusfariasdasneves@gmail.com>
2024-08-03 09:57:56 -0300

    feat: add Toaster config

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import ReactDOM from "react-dom/client"
 import { QueryClient, QueryClientProvider } from "react-query"
 
 import App from "./App.tsx"
+import { Toaster } from "./components/ui/sonner.tsx"
 
 const queryClient = new QueryClient()
 
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <Toaster />
     </QueryClientProvider>
   </React.StrictMode>,
 )
